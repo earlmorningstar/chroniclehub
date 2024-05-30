70d28753a3f3ad04867afee91d13e9e7bad387a9
Onyeabor Joel <earlmorningstar@gmail.com>
2024-05-30 16:10:59 +0100

    Update ArticlesContext.jsx

---
+++ api/news.js
@@ -0,0 +1,33 @@
+// // api/news.js
+// import fetch from "node-fetch";
+
+// const fetchNews = async (req, res) => {
+//   const { query } = req;
+//   const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+//   let apiUrl = `https://newsapi.org/v2/top-headlines`;
+
+//   if (query.sources) {
+//     apiUrl += `?sources=${query.sources}`;
+//   } else if (query.country && query.category) {
+//     apiUrl += `?country=${query.country}&category=${query.category}`;
+//   } else if (query.country) {
+//     apiUrl += `?country=${query.country}`;
+//   } else if (query.q) {
+//     apiUrl = `https://newsapi.org/v2/top-headlines?q=${query.q}`;
+//   } else if (query.everything) {
+//     apiUrl = `https://newsapi.org/v2/everything?q=${query.everything}`;
+//     if (query.from && query.to && query.sortBy) {
+//       apiUrl += `&from=${query.from}&to=${query.to}&sortBy=${query.sortBy}`;
+//     }
+//   } else if (query.domains) {
+//     apiUrl = `https://newsapi.org/v2/everything?domains=${query.domains}`;
+//   }
+
+//   apiUrl += `&apiKey=${apiKey}`;
+
+//   const response = await fetch(apiUrl);
+//   const data = await response.json();
+//   res.status(response.status).json(data);
+// };
+
+// export default fetchNews;
--- src/store/ArticlesContext.jsx
+++ src/store/ArticlesContext.jsx
@@ -87,3 +87,92 @@ export const ArticlesProvider = ({ children }) => {
 };
 
 export default ArticlesContext;
+
+
+// import { createContext, useState, useEffect } from "react";
+// import { formatDistanceToNow, parseISO } from "date-fns";
+
+// const ArticlesContext = createContext();
+
+// export const ArticlesProvider = ({ children }) => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         setLoading(true);
+
+//         const apiUrls = [
+//           `/api/news?country=us`,
+//           `/api/news?sources=bbc-news`,
+//           `/api/news?country=de&category=business`,
+//           `/api/news?q=trump`,
+//           `/api/news?everything=bitcoin`,
+//           `/api/news?everything=apple&from=2024-05-15&to=2024-05-15&sortBy=popularity`,
+//           `/api/news?domains=techcrunch.com,thenextweb.com`,
+//         ];
+
+//         let allArticles = [];
+//         const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+//         const now = new Date();
+
+//         for (let url of apiUrls) {
+//           const response = await fetch(url);
+//           if (response.ok) {
+//             const data = await response.json();
+//             const articles = data.articles
+//               .filter((article) => {
+//                 const publishedAt = new Date(article.publishedAt);
+//                 const ageInDays = (now - publishedAt) / (1000 * 60 * 60 * 24);
+//                 return (
+//                   ageInDays <= 2 &&
+//                   article.title &&
+//                   article.description &&
+//                   article.author &&
+//                   article.urlToImage &&
+//                   article.title !== "[Removed]" &&
+//                   article.description !== "[Removed]" &&
+//                   article.author !== "[Removed]" &&
+//                   article.publishedAt !== "[Removed]" &&
+//                   article.urlToImage !== "[Removed]"
+//                 );
+//               })
+//               .map((article) => ({
+//                 ...article,
+//                 publishedAt: formatDistanceToNow(
+//                   parseISO(article.publishedAt),
+//                   { addSuffix: true }
+//                 ),
+//               }));
+//             allArticles = allArticles.concat(articles);
+//             await delay(1000);
+//           } else {
+//             console.error("Failed to fetch articles from:", url);
+//           }
+//         }
+
+//         setArticles(allArticles);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error("Error fetching articles:", error);
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+
+//     const interval = setInterval(fetchData, 15 * 60 * 1000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <ArticlesContext.Provider value={{ articles, loading }}>
+//       {children}
+//     </ArticlesContext.Provider>
+//   );
+// };
+
+// export default ArticlesContext;
